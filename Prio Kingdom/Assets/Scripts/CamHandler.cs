5b4e5b2697159aa5246d40efcf1c7f7ee3a17a96
Onur Göğebakan <ggbknonur@gmail.com>
2022-03-13 21:22:43 +0300

    pitch zoom

--- Prio Kingdom/Assets/Scripts/CamHandler.cs
+++ Prio Kingdom/Assets/Scripts/CamHandler.cs
@@ -8,7 +8,8 @@ public class CamHandler : MonoBehaviour
 
     private bool onDrag;
 
-    private float camSpeed = 0.01f;
+    private float camSpeed = 2.5f;
+
 
     void Start()
     {
@@ -19,7 +20,7 @@ public class CamHandler : MonoBehaviour
     void Update()
     {
 
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
             RaycastHit hit;
@@ -40,14 +41,38 @@ public class CamHandler : MonoBehaviour
             if (Physics.Raycast(ray, out hit))
             {
                 dragPos = new Vector3(Input.GetTouch(0).deltaPosition.x * camSpeed, 0, Input.GetTouch(0).deltaPosition.y * camSpeed);
-                dragPos = Quaternion.Euler(0, 45, 0) * dragPos;
+                dragPos = Quaternion.Euler(0, 45, 0) * dragPos * Time.deltaTime;
                 transform.position = new Vector3(transform.position.x - dragPos.x, transform.position.y, transform.position.z - dragPos.z);
             }
         }
 
-        if (onDrag && (Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetTouch(0).phase == TouchPhase.Canceled))
+        if (onDrag && (Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetTouch(0).phase == TouchPhase.Canceled) || Input.touchCount != 1)
         {
             onDrag = false;
+        }
+
+
+        if (Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+            float difference = (currentMagnitude - prevMagnitude) * 2;
+
+            if ((transform.position.y >60 && difference < 0) || (transform.position.y < 15 && difference > 0))
+            {
+                difference = 0;
+            }
+
+            transform.Translate(new Vector3(0, 0, difference) * Time.deltaTime, Space.Self);
+
+
         }
     }
 }
